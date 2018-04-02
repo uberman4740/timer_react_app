bb994e9377c4b2159aa54bd352be32bea2a9cfc0
KS <ksagoo.ee@gmail.com>
2018-04-02 05:49:51 -0700

    testing semantic-works!

--- src/App.js
+++ src/App.js
@@ -5,15 +5,27 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div>
+            <div class="ui three buttons">
+                <button class="ui active button">One</button>
+                <button class="ui button">Two</button>
+                <button class="ui button">Three</button>
+            </div>
+
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo" />
+                        <h1 className="App-title">Welcome to React</h1>
+                    </header>
+                    <p className="App-intro">
+                        To get started, edit <code>src/App.js</code> and save to reload.
+                    </p>
+                </div>
+
+
+        </div>
+
+
     );
   }
 }
