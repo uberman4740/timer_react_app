94effcae1004342bf684c8818c723480b1db5ee2
KS <ksagoo.ee@gmail.com>
2018-04-02 06:10:21 -0700

    static component- EditableTimerList

--- src/App.js
+++ src/App.js
@@ -13,11 +13,34 @@ class TimersDashboard extends Component{
     }
 }
 
+
+class EditableTimerList extends Component{
+    render(){
+        return(
+            <div>
+                <EditableTimer
+                    title="Morning Coding"
+                    project="Personal Development"
+                    elapsed="0000001"
+                    runningSince={null}
+                />
+                <EditableTimer
+                    title="Cooking Breakfast"
+                    project="Food"
+                    elapsed="0000012"
+                    runningSince={null}
+                    editFormOpen={true}
+                />
+
+            </div>
+        );
+    }
+}
 class App extends Component {
   render() {
     return (
         <div>
-
+            <TimersDashboard/>
 
 
 
